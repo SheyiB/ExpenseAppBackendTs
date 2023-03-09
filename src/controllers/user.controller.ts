103ba8fcbcd18eadfae547c1fbf5fd423db2a086
Seyi-Banjo <elijahbanjo@gmail.com>
2023-03-09 12:04:04 +0100

    User Controller Update [Next + TypeScriptify ]

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,49 +1,54 @@
 import {UserService} from '../services/user.service';
-import {Request, Response} from "express";
+import {Request, Response, NextFunction} from "express";
 
 const User = new UserService();
 
-export const createUser = async (req: Request, res: Response, next) =>{
+export const createUser = async (req: Request, res: Response, next : NextFunction) =>{
     try{
         const user = await User.createUser(req.body);
+        next()
         return res.status(201).json(user)
-    }  catch(e){
+    }  catch(e:any){
         return res.status(500).json(e.message)
     }
 }
 
-export const getAllUsers = async (req: Request, res: Response, next) =>{
+export const getAllUsers = async (req: Request, res: Response, next : NextFunction) =>{
     try{
         const user = await User.getAllUser();
+        next()
         return res.status(200).json(user)
-    }  catch(e){
+    }  catch(e:any){
         return res.status(500).json(e)
     }
 }
 
-export const getUser = async (req: Request, res: Response, next) =>{
+export const getUser = async (req: Request, res: Response, next : NextFunction) =>{
     try{
         const user = await User.getUser(req.params.id);
+        next()
         return res.status(200).json(user)
-    }  catch(e){
+    }  catch(e:any){
         return res.status(500).json(e)
     }
 }
 
-export const updateUser = async (req: Request, res: Response, next) =>{
+export const updateUser = async (req: Request, res: Response, next : NextFunction) =>{
     try{
         const user = await User.updateUser(req.params.id, req.body);
+        next()
         return res.status(200).json(user)
-    }  catch(e){
+    }  catch(e:any){
         return res.status(500).json(e)
     }
 }
 
-export const deleteUser = async (req: Request, res: Response, next) =>{
+export const deleteUser = async (req: Request, res: Response, next : NextFunction) =>{
     try{
         const user = await User.deleteUser(req.params.id);
+        next()
         return res.status(204).json({data: user})
-    }  catch(e){
+    }  catch(e:any){
         return res.status(500).json(e)
     }
 }
