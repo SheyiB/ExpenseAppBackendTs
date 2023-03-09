70bc95452fe414b346b867e369cf263fb959a0db
Seyi-Banjo <elijahbanjo@gmail.com>
2023-03-09 13:30:44 +0100

    User Controller  Next Middleware fix

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -6,49 +6,44 @@ const User = new UserService();
 export const createUser = async (req: Request, res: Response, next : NextFunction) =>{
     try{
         const user = await User.createUser(req.body);
-        next()
-        return res.status(201).json(user)
+        return next(res.status(201).json(user))
     }  catch(e:any){
-        return res.status(500).json(e.message)
+        return next(res.status(500).json(e.message))
     }
 }
 
 export const getAllUsers = async (req: Request, res: Response, next : NextFunction) =>{
     try{
         const user = await User.getAllUser();
-        next()
-        return res.status(200).json(user)
+        return next(res.status(200).json(user))
     }  catch(e:any){
-        return res.status(500).json(e)
+        return next(res.status(500).json(e))
     }
 }
 
 export const getUser = async (req: Request, res: Response, next : NextFunction) =>{
     try{
         const user = await User.getUser(req.params.id);
-        next()
-        return res.status(200).json(user)
+        return next(res.status(200).json(user))
     }  catch(e:any){
-        return res.status(500).json(e)
+        return next(res.status(500).json(e))
     }
 }
 
 export const updateUser = async (req: Request, res: Response, next : NextFunction) =>{
     try{
         const user = await User.updateUser(req.params.id, req.body);
-        next()
-        return res.status(200).json(user)
+        return next(res.status(200).json(user))
     }  catch(e:any){
-        return res.status(500).json(e)
+        return next(res.status(500).json(e))
     }
 }
 
 export const deleteUser = async (req: Request, res: Response, next : NextFunction) =>{
     try{
         const user = await User.deleteUser(req.params.id);
-        next()
         return res.status(204).json({data: user})
     }  catch(e:any){
-        return res.status(500).json(e)
+        return next(res.status(500).json(e))
     }
 }
