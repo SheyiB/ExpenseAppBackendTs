4f5fb9a1634f0a519f2e42ab4676da6c0410ad4d
Seyi-Banjo <elijahbanjo@gmail.com>
2023-03-10 13:15:20 +0100

    Purchase Controller Update [Error Handling]

--- src/controllers/purchase.controller.ts
+++ src/controllers/purchase.controller.ts
@@ -9,8 +9,7 @@ export const createPurchase = async (req: Request, res: Response, next: NextFunc
         return next(res.status(201).json(purchase)); 
         
     }  catch(e:any){
-        
-        return next(res.status(500).json(e.message))
+        return next(res.status(e.code? e.code : 500).json({success: false, message: e.message}))
     }
 }
 
@@ -51,7 +50,7 @@ export const updatePurchase = async (req: Request, res: Response, next: NextFunc
         
         return next(res.status(200).json(purchase))
     }  catch(e:any){
-        return next(res.status(500).json(e))
+        return next(res.status(e.code? e.code : 500).json({success: false, message: e.message}))
     }
 }
 
