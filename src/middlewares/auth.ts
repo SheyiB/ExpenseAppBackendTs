2dd60f835e5372ab6eb58769fba3713b41939c16
Seyi-Banjo <elijahbanjo@gmail.com>
2023-03-10 13:08:35 +0100

    Auth Middleware Update [Next]

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -6,7 +6,7 @@ export const protect = async(req: Request, res: Response, next: NextFunction) =>
 
 	const token = req.header('x-auth-token');
 
-	if(!token) return res.status(401).json({message: 'UNAUTHORIZED!'})
+	if(!token) return next(res.status(401).json({message: 'UNAUTHORIZED!'}))
 
 	try{
 
@@ -16,25 +16,25 @@ export const protect = async(req: Request, res: Response, next: NextFunction) =>
 
 		const currentUser = await User.findById(id);
 
-		if(!currentUser) return res.status(404).json({message: 'USER NOT FOUND!'});
+		if(!currentUser) return next(res.status(404).json({message: 'USER NOT FOUND!'}));
 
 		currentUser.password = '';
 
 		(req as any).user = currentUser;
 
 
-		next();
-		return;
+		return next();
+		
 
 
 	}catch(e: any){
 
 		if (e.name === 'TokenExpiredError' ){
-			return res.status(403).json({message: 'TOKEN EXPIRED! LOGIN!'});
+			return next(res.status(403).json({message: 'TOKEN EXPIRED! LOGIN!'}));
 		}
 
 		else {
-			return res.status(406).json({message: 'INVALID TOKEN! '})
+			return next(res.status(406).json({message: 'INVALID TOKEN! '}))
 		} 
 	}	
 
