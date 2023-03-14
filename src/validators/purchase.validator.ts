80e0221e6b7ed948b0a4471c27f2d48c16daae2d
Seyi-Banjo <elijahbanjo@gmail.com>
2023-03-14 16:42:15 +0100

    Purchase Validator -> validateCreatePurchase + validateUpdatePurchase

---
+++ src/validators/purchase.validator.ts
@@ -0,0 +1,32 @@
+import Joi from 'joi'
+import {IPurchase} from '../models/purchase'
+
+function validateCreatePurchase( purchaseDetails : IPurchase){
+    const createPurchaseSchema = Joi.object().keys({
+        item: Joi.string().required(),
+        price: Joi.number().required(),
+        category: Joi.string().required(),
+        date: Joi.date().required(),
+        user: Joi.string().required()
+
+    })
+
+    return createPurchaseSchema.validate(purchaseDetails)
+}
+
+function validateUpdatePurchase( purchaseDetails : IPurchase){
+    const updatePurchaseSchema = Joi.object().keys({
+        item: Joi.string().optional(),
+        price: Joi.number().optional(),
+        category: Joi.string().optional(),
+        date: Joi.date().optional(),
+        user: Joi.string().optional()
+    })
+
+    return updatePurchaseSchema.validate(purchaseDetails)
+}
+
+module.exports = {
+    validateCreatePurchase,
+    validateUpdatePurchase
+}
