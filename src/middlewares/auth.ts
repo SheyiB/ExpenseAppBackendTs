e1b1f90f27a916a59d49a762febcbdd6813bdc2f
Seyi-Banjo <elijahbanjo@gmail.com>
2023-03-17 22:08:45 +0100

    Auth Update - role checking

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -9,11 +9,8 @@ export const protect = async(req: Request, res: Response, next: NextFunction) =>
 	if(!token) return next(res.status(401).json({message: 'UNAUTHORIZED!'}))
 
 	try{
-
 		const decoded =  jwt.verify(token, process.env.JWT_SECRET!);
-
 		const { id } = (decoded as any)
-
 		const currentUser = await User.findById(id);
 
 		if(!currentUser) return next(res.status(404).json({message: 'USER NOT FOUND!'}));
@@ -26,6 +23,46 @@ export const protect = async(req: Request, res: Response, next: NextFunction) =>
 		return next();
 		
 
+
+	}catch(e: any){
+
+		if (e.name === 'TokenExpiredError' ){
+			return next(res.status(403).json({message: 'TOKEN EXPIRED! LOGIN!'}));
+		}
+
+		else {
+			return next(res.status(406).json({message: 'INVALID TOKEN! '}))
+		} 
+	}	
+
+}
+
+export const isAccountOwner = async(req: Request, res: Response, next: NextFunction) => {
+
+	
+	const token = req.header('x-auth-token');
+
+	if(!token) return next(res.status(401).json({message: 'UNAUTHORIZED!'}))
+
+	try{
+
+		const decoded =  jwt.verify(token, process.env.JWT_SECRET!);
+
+		const { id } = (decoded as any)
+
+		const currentuser = (req as any).user ;
+
+		if(!currentuser){
+			console.log("Crashhh")
+		}
+		else{
+			if (id != req.params.id || currentuser.role != 'SuperUser'){
+				return res.status(401).json({message: 'UNAUTHORIZED USER!'})
+			}	
+		}
+		
+		return next();
+		
 
 	}catch(e: any){
 
